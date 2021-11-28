3525db8102c89e217f8b0224784ffd1b93b11ee7
Hyuntae <bht9011@gmail.com>
2021-11-28 11:30:38 +0900

    new repo for react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
